f7ca3f7f72f22af85c6fcc3386d35cc5ba6f036d
MrGluster <mrgluster@gmail.com>
2016-01-12 13:28:12 +0100

    second commit
    här kommer projectet med alla mappar och grejer

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+			//Pickup??
+		if (Input.GetButtonDown("Pickup")
+			print("space key was pressed");
+
+			//Physics.Raycast(transform.position, transform.forward, hit
+			//Debug.DrawRay(transform.position, forward, Color.green);
+		//skjuta ut en ray mot lådan, kolla hur nära lådan är,
+		//om lådan är tillräckligt nära lyft upp den(Ta bort en komponent, och lägga till en, för att sedan lägga till den första igen,), 
+	}
+}
